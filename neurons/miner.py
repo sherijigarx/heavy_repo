8dd34356b9ddbfb506ae0f0873a677e02fbd5072
isharab <155580193+sherijigarx@users.noreply.github.com>
2024-02-09 01:02:57 +0500

    Update miner.py

--- neurons/miner.py
+++ neurons/miner.py
@@ -187,6 +187,7 @@ def main(config):
                         
     # =========================================== Voice Clone model selection ===============================================    
         if config.bark_vc_path or config.clone_model == "bark/voiceclone":
+            bt.logging.info(f"Checking the path of the model supplies: {config.bark_vc_path}")
             bark_vc_model = config.bark_vc_path if config.bark_vc_path else config.clone_model
             bt.logging.info(f"Using the Voice Clone with the supplied model: {bark_vc_model}")
             voice_clone_model = ModelLoader(model_dir=bark_vc_model)
